30f36bf5611c07758f76dfacb22c7af3a7a2aff7
Rick Lau <liuh01.cn@gmail.com>
2024-04-09 10:57:46 +0800

    feat 调整结束页

--- app/end/page.tsx
+++ app/end/page.tsx
@@ -1,18 +1,37 @@
 'use client'
-
 import {Box, Flex, Strong, Text} from "@radix-ui/themes";
 import Link from "next/link";
+import {useEffect, useState} from "react";
 export default function Home() {
+
+  const [second, setSecond] = useState(10)
+
+  useEffect(() => {
+    if(second > 0) {
+      const timerId = setTimeout(() => {
+        setSecond(second - 1);
+      }, 1000);
+      return () => clearTimeout(timerId);
+    } else {
+      window.location.href = 'https://www.senixtools.com'
+    }
+  },[second])
   return (
     <Flex width="100%" height="100%" align="center" justify="between" direction="column">
       <Flex align="center" justify="center" width="100%" height="9" mt="9">
         <img className="w-48" src="/logo.png" alt="senix logo image"/>
       </Flex>
       <Flex grow="1" direction="column" align="center" justify="center" width="100%">
-        <Flex direction="column" align="center" justify="between"
-          className="w-80 h-40 border-4 rounded-xl border-[rgb(var(--background-senix-orange-rgb))] bg-white p-">
-          <Text weight="medium" size="5">The raffle has ended</Text>
-          <Text weight="regular" size="4">You will be redirected to the official website in 10 seconds.</Text>
+        <Flex direction="column" align="center" justify="start"
+          className="w-80 h-40 border-4 rounded-xl border-[rgb(var(--background-senix-orange-rgb))] bg-white">
+          <Box className="mt-4">
+            <Text weight="medium" size="5" className="text-orange-600">The raffle has ended</Text>
+          </Box>
+          <Box className="mt-2 px-2">
+            <Text weight="regular" size="3" className="px-2">You will be redirected to the official website in
+              <Text weight="regular" size="3" className="text-orange-600 pl-1">{second} </Text> seconds.
+            </Text>
+          </Box>
         </Flex>
       </Flex>
 
