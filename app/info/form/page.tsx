02e3c120a1effcf9655a9df61d90d36fe91e976c
Rick Lau <liuh01.cn@gmail.com>
2024-04-09 14:19:41 +0800

    fix fix dev error.

--- app/info/form/page.tsx
+++ app/info/form/page.tsx
@@ -6,8 +6,10 @@ import {FormEvent} from "react";
 interface HomeProps {
   onSubmit: (event: FormEvent<HTMLFormElement>) => void;
 }
-export default function Home({onSubmit}: HomeProps) {
-  return (<Form.Root className="w-full" onSubmit={(event) => { onSubmit(event) }}>
+
+export default ({onSubmit}: HomeProps) => (<Form.Root className="w-full" onSubmit={(event) => {
+    onSubmit(event)
+  }}>
     <Form.Field className="grid mb-[10px]" name="code">
       <div className="flex items-baseline justify-between">
         <Form.Label className="text-[15px] font-medium leading-[35px] text-white">SN Code</Form.Label>
@@ -43,7 +45,8 @@ export default function Home({onSubmit}: HomeProps) {
         <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
           Phone Number
         </Form.Label>
-        <Form.Message className="text-[13px] text-[rgb(var(--background-senix-orange-rgb))] opacity-[0.8]" match="valueMissing">
+        <Form.Message className="text-[13px] text-[rgb(var(--background-senix-orange-rgb))] opacity-[0.8]"
+                      match="valueMissing">
           Please enter your phone number
         </Form.Message>
       </div>
@@ -55,9 +58,10 @@ export default function Home({onSubmit}: HomeProps) {
       </Form.Control>
     </Form.Field>
     <Form.Submit asChild>
-      <button className="box-border w-full text-white shadow-blackA4 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-[rgb(var(--background-senix-orange-rgb))] px-[15px] font-medium leading-none shadow-[0_2px_10px] mt-[10px]">
+      <button
+        className="box-border w-full text-white shadow-blackA4 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-[rgb(var(--background-senix-orange-rgb))] px-[15px] font-medium leading-none shadow-[0_2px_10px] mt-[10px]">
         Lucky draw
       </button>
     </Form.Submit>
   </Form.Root>
-  )};
+);
